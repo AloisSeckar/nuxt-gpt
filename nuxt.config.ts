c942a80841bcb0399a3c0434ed0df375f48a08d7
Alois Sečkár <ellrohir@seznam.cz>
2025-11-11 21:44:40 +0100

    build: update deps after a while

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-01-09',
+  compatibilityDate: '2025-11-11',
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
